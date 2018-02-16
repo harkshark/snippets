f7ed15f927b37479855cc795c12dc2c94aa19c8f
Lauren Rodriguez <harkshark@users.noreply.github.com>
2018-02-16 17:06:02 -0500

    Fix staged changes prompt

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -15,7 +15,7 @@ override_git_prompt_colors() {
   GIT_PROMPT_SYMBOLS_AHEAD="${White}${DIM}↟"
   GIT_PROMPT_SYMBOLS_BEHIND="${White}${DIM}↡"
   GIT_PROMPT_SEPARATOR=""
-  GIT_PROMPT_STAGED="${Green}${DIM}+"
+  GIT_PROMPT_STAGED=" ${Green}${DIM}+"
   GIT_PROMPT_CONFLICTS="${Red}${DIM}"
   GIT_PROMPT_CHANGED=" ${Red}${DIM}!"
   GIT_PROMPT_UNTRACKED="${Red}${DIM}?"
