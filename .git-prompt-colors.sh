bcc8758fee65d449105283a9346d23d5d792b007
Lauren Rodriguez <harkshark@users.noreply.github.com>
2018-03-01 11:46:48 -0500

    Undimmed conflicts indicator

--- .git-prompt-colors.sh
+++ .git-prompt-colors.sh
@@ -16,7 +16,7 @@ override_git_prompt_colors() {
   GIT_PROMPT_SYMBOLS_BEHIND="${White}${DIM}↡"
   GIT_PROMPT_SEPARATOR=""
   GIT_PROMPT_STAGED=" ${Green}${DIM}+"
-  GIT_PROMPT_CONFLICTS="${Red}${DIM}"
+  GIT_PROMPT_CONFLICTS=" ${Red}﹟"
   GIT_PROMPT_CHANGED=" ${Red}${DIM}!"
   GIT_PROMPT_UNTRACKED="${Red}${DIM}?"
   GIT_PROMPT_STASHED=" ${Yellow}${DIM}⍥"
@@ -25,7 +25,7 @@ override_git_prompt_colors() {
   GIT_PROMPT_START_USER="🌱 ${Yellow}${PathShort}${ResetColor}"
   GIT_PROMPT_END_USER=" $ "
   GIT_PROMPT_END_ROOT="${BoldRed} # ${ResetColor}"
-  GIT_PROMPT_SYMBOLS_NO_REMOTE_TRACKING="${Magenta}${DIM}⍏"
+  GIT_PROMPT_SYMBOLS_NO_REMOTE_TRACKING="${Magenta}${DIM}⍏ "
 }
 
 reload_git_prompt_colors "Single_line_Solarized_Lauren"
